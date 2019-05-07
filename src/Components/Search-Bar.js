049cdd48ed6c3021e43edcd56b89789441dc1d56
Stefan Wurck <vitatech@gmx.de>
2019-05-07 12:32:55 +0200

    mistakes

--- src/Components/Search-Bar.js
+++ src/Components/Search-Bar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBar = props => {
   return (
-    <input
+    < input
       value={props.searchTerm}
       onChange={props.onSearchInputChangeHandler}
       placeholder="Search to find music"
